0c93a1e2e9cfad1853a94799bba94e668be98efa
elvisalibaba <117951682+elvisalibaba@users.noreply.github.com>
2026-02-17 14:58:11 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -82,7 +82,7 @@ export default function Header() {
             rel="noopener noreferrer"
             className="flex items-center gap-2 bg-[#E50914] text-white px-4 py-2 rounded-md shadow-md hover:bg-[#b00710] hover:shadow-lg hover:scale-105 transition-all duration-300"
           >
-            <span>Devis WhatsApp</span>
+            <span>Prendre rendez-vous</span>
           </a>
         </div>
 
